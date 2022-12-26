990dec5db0e634c951d8b2bad42769ab362205df
uurha <uurh2man@gmail.com>
2022-12-26 01:42:10 +0100

    Move tools

---
+++ Assets/BetterEditorTools/Editor/EditorTools/Helpers/DropDown/DropdownBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace Better.EditorTools.Helpers.DropDown
+{
+    public abstract class DropdownBase
+    {
+        protected DropdownBase(GUIContent content)
+        {
+            Content = content;
+        }
+
+        public GUIContent Content { get; }
+        internal abstract bool Invoke(DropdownWindow downPopup);
+
+        public bool Equals(string value)
+        {
+            return Content.text.Equals(value);
+        }
+
+
+        public bool Contains(string searchText,
+            StringComparison comparison = StringComparison.OrdinalIgnoreCase)
+        {
+#if UNITY_2021_1_OR_NEWER
+            return Content.text.Contains(searchText, comparison);
+#else
+            return Content.text.IndexOf(searchText, 0, StringComparison.OrdinalIgnoreCase) != -1;
+#endif
+        }
+    }
+}
---
+++ Assets/BetterEditorTools/Editor/EditorTools/Helpers/DropDown/DropdownSubTree.cs
@@ -0,0 +1,25 @@
+﻿using Better.DataStructures.Tree;
+using UnityEngine;
+
+namespace Better.EditorTools.Helpers.DropDown
+{
+    public class DropdownSubTree : DropdownBase, INodeValue<DropdownBase>
+    {
+        private TreeNode<DropdownBase> _node;
+
+        public DropdownSubTree(GUIContent content) : base(content)
+        {
+        }
+
+        public void SetNode(TreeNode<DropdownBase> node)
+        {
+            _node = node;
+        }
+
+        internal override bool Invoke(DropdownWindow downPopup)
+        {
+            downPopup.SetCurrentDrawItems(_node);
+            return false;
+        }
+    }
+}
