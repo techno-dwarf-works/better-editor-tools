2db9567a776916c7d28139ca11d3598ec9db8baf
uurha <uurha@users.noreply.github.com>
2022-12-26 00:45:27 +0000

    Update README.md and LICENSE

---
+++ Editor/EditorTools/Helpers/DropDown/DropdownItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Better.EditorTools.Helpers.DropDown
+{
+    public class DropdownItem : DropdownBase
+    {
+        private readonly Action<object> _onSelect;
+        private readonly object _object;
+
+        public DropdownItem(GUIContent content, Action<object> onSelect, object data) : base(content)
+        {
+            _onSelect = onSelect;
+            _object = data;
+        }
+
+        internal override bool Invoke(DropdownWindow downPopup)
+        {
+            _onSelect?.Invoke(_object);
+            return true;
+        }
+    }
+}
