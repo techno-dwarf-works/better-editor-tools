c260215b20633c3d7bf3d28635b8976e73217db1
uurha <uurha@users.noreply.github.com>
2023-04-04 10:46:46 +0000

    Merge pull request #7 from uurha/dev
    Dev

---
+++ Editor/EditorTools/Comparers/BaseComparer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Better.EditorTools.Comparers
+{
+    public abstract class BaseComparer<T, U> where T : IEqualityComparer<U>, new()
+    {
+        public static T Instance { get; } = new T();
+    }
+}
---
+++ Editor/EditorTools/Drawers/Base/WrappersTypeCollection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Better.EditorTools.Drawers.Base
+{
+    public class WrappersTypeCollection : Dictionary<Type, Dictionary<Type, Type>>
+    {
+        public WrappersTypeCollection() : base()
+        {
+        }
+
+        public WrappersTypeCollection(IEqualityComparer<Type> equalityComparer) : base(equalityComparer)
+        {
+        }
+    }
+}
