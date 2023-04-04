c0d417a8e204d1d4dd0f792a061f56bf167ad5d2
uurha <uurhaa@gmail.com>
2023-04-04 12:34:42 +0200

    Add base drawers

---
+++ Assets/BetterEditorTools/Editor/EditorTools/Drawers/Base/WrapperCollectionValue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Better.EditorTools.Utilities;
+
+namespace Better.EditorTools.Drawers.Base
+{
+    public class WrapperCollectionValue<T> where T : UtilityWrapper
+    {
+        public WrapperCollectionValue(T wrapper, Type type)
+        {
+            Wrapper = wrapper;
+            Type = type;
+        }
+
+        public T Wrapper { get; }
+        public Type Type { get; }
+    }
+}
