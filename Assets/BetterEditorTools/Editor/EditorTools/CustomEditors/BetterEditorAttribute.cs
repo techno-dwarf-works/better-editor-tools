3381822e7c321d84538fee9ea790e62ea39923e9
uurhaa <uurha.dev@gmail.com>
2023-04-23 07:05:47 +0200

    Add base for MultiEditor.cs

---
+++ Assets/BetterEditorTools/Editor/EditorTools/CustomEditors/BetterEditorAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Better.EditorTools.CustomEditors
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class BetterEditorAttribute : Attribute
+    {
+        public Type EditorFor { get; }
+        public bool EditorForChildClasses { get; }
+        public bool OverrideDefaultEditor { get; set; }
+        public int Order { get; set; }
+
+        public BetterEditorAttribute(Type editorFor, bool editorForChildClasses)
+        {
+            EditorFor = editorFor;
+            EditorForChildClasses = editorForChildClasses;
+        }
+    }
+}
