379f296ec9648d2214970ed6e5d889efe499e50f
uurhaa <uurha.dev@gmail.com>
2023-05-14 10:10:41 +0200

    Add more methods for SerializedPropertyExtensions.cs

--- Assets/BetterEditorTools/Editor/EditorTools/SerializedPropertyDefines.cs
+++ Assets/BetterEditorTools/Editor/EditorTools/SerializedPropertyDefines.cs
@@ -4,8 +4,11 @@ namespace Better.EditorTools
 {
     public static class SerializedPropertyDefines
     {
-        public static readonly Regex ArrayDataRegex = new Regex(@"\.Array\.data\[[0-9]+\]", RegexOptions.Compiled);
+        public static readonly Regex ArrayDataWithIndexRegex = new Regex(@"\.Array\.data\[[0-9]+\]", RegexOptions.Compiled);
 
         public static readonly Regex ArrayElementRegex = new Regex(@"\GArray\.data\[(\d+)\]", RegexOptions.Compiled);
+        public static readonly Regex ArrayIndexRegex = new Regex(@"\[([^\[\]]*)\]", RegexOptions.Compiled);
+        
+        public static readonly Regex ArrayRegex = new Regex(@"\.Array\.data", RegexOptions.Compiled);
     }
 }
