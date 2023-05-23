59a0a1751e6a7ab00900719ae55b38c9ebcc5e58
uurha <uurha@users.noreply.github.com>
2023-05-23 14:30:29 +0000

    Merge pull request #32
    dev

--- Editor/EditorTools/Drawers/Base/MultiFieldDrawer.cs
+++ Editor/EditorTools/Drawers/Base/MultiFieldDrawer.cs
@@ -4,11 +4,27 @@ using Better.EditorTools.Utilities;
 using Better.Extensions.Runtime;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Pool;
 
 namespace Better.EditorTools.Drawers.Base
 {
     public abstract class MultiFieldDrawer<T> : FieldDrawer where T : UtilityWrapper
     {
+        private static readonly Cache CacheField = new Cache();
+        
+        protected class Cache
+        {
+            public bool IsValid { get; private set; }
+            public WrapperCollectionValue<T> Value { get; private set; }
+
+            public Cache Set(bool isValid, WrapperCollectionValue<T> value)
+            {
+                IsValid = isValid;
+                Value = value;
+                return this;
+            }
+        }
+
         protected WrapperCollection<T> _wrappers;
 
         /// <summary>
@@ -36,6 +52,7 @@ namespace Better.EditorTools.Drawers.Base
             return fieldInfo.FieldType;
         }
 
+
         /// <summary>
         /// Validates if <see cref="_wrappers"/> contains property by <see cref="SerializedPropertyComparer"/>
         /// </summary>
@@ -43,21 +60,26 @@ namespace Better.EditorTools.Drawers.Base
         /// <param name="handler"><see cref="BaseUtility{THandler}"/> used to validate current stored wrappers and gets instance for recently added property</param>
         /// <typeparam name="THandler"></typeparam>
         /// <returns>Returns true if wrapper for <paramref name="property"/> was already stored into <see cref="_wrappers"/></returns>
-        protected bool ValidateCachedProperties<THandler>(SerializedProperty property, BaseUtility<THandler> handler) where THandler : new()
+        protected Cache ValidateCachedProperties<THandler>(SerializedProperty property, BaseUtility<THandler> handler)
+            where THandler : new()
         {
             var fieldType = GetFieldOrElementType();
-            var contains = _wrappers.ContainsKey(property);
-            if (contains)
+            if (_wrappers.TryGetValue(property, out var wrapperCollectionValue))
             {
                 handler.ValidateCachedProperties(_wrappers);
-            }
-            else
-            {
-                var gizmoWrapper = handler.GetUtilityWrapper<T>(fieldType, attribute.GetType());
-                _wrappers.Add(property, new WrapperCollectionValue<T>(gizmoWrapper, fieldType));
+                return CacheField.Set(true, wrapperCollectionValue);
             }
 
-            return contains;
+            var wrapper = handler.GetUtilityWrapper<T>(fieldType, attribute.GetType());
+            if (wrapper == null)
+            {
+                return CacheField.Set(false, null);
+            }
+
+            var collectionValue = new WrapperCollectionValue<T>(wrapper, fieldType);
+            _wrappers.Add(property, collectionValue);
+
+            return CacheField.Set(false, collectionValue);
         }
     }
 }
