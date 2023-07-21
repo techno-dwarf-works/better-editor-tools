fdad695b017c8ab39a3bdaf3619d18cd1c4d4d54
uurhaa <uurha.dev@gmail.com>
2023-07-21 03:52:32 +0200

    Add ReorderableListHelpers.cs

---
+++ Assets/BetterEditorTools/Editor/EditorTools/Helpers/ReorderableListHelpers.cs
@@ -0,0 +1,23 @@
+﻿using Better.Tools.Runtime;
+using UnityEditor;
+using UnityEditorInternal;
+
+namespace Better.EditorTools.Helpers
+{
+    public static class ReorderableListHelpers
+    {
+        private static System.Reflection.MethodInfo _repaintInspectors = null;
+
+        //TODO: Need to find better way to refresh ReorderableList
+        public static void RepaintAllInspectors(SerializedProperty property)
+        {
+            if (_repaintInspectors == null)
+            {
+                var inspWin = typeof(ReorderableList);
+                _repaintInspectors = inspWin.GetMethod("InvalidateParentCaches", BetterEditorDefines.MethodFlags);
+            }
+
+            if (_repaintInspectors != null) _repaintInspectors.Invoke(null, new object[] { property.propertyPath });
+        }
+    }
+}
