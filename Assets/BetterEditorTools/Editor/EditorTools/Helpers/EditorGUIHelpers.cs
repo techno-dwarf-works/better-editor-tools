658828d8fd048f9a28339bd00c35f0578993c014
uurhaa <uurha.dev@gmail.com>
2023-07-29 02:49:38 +0200

    Add Safe drawing for property field

---
+++ Assets/BetterEditorTools/Editor/EditorTools/Helpers/EditorGUIHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Better.Tools.Runtime;
+using UnityEditor;
+using UnityEngine;
+
+namespace Better.EditorTools.Helpers
+{
+    public static class EditorGUIHelpers
+    {
+        private static MethodInfo _defaultPropertyField;
+        public static bool PropertyFieldSafe(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (_defaultPropertyField == null)
+            {
+                var type = typeof(EditorGUI);
+                _defaultPropertyField = type.GetMethod("DefaultPropertyField", BetterEditorDefines.MethodFlags);
+            }
+
+            return (bool)_defaultPropertyField.Invoke(null, new object[] { position, property, label });
+        }
+    }
+}
